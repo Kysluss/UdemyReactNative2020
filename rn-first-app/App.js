c63e138d1d1edeb5c36749e8e4c1e2b076a6cca5
Kyle Slusser <Kysluss@aol.com>
2020-02-12 19:58:15 -0500

    put changes back

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -5,7 +5,7 @@ export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{outputText}</Text>
       <Button title="Change Text" onPress={() => setOutputText('The text changed!')} />
     </View>
@@ -13,5 +13,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
